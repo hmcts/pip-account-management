3f53b9b9e06e25aeec31e138b28a39512d338934
Daniel Furnivall <danielfurnivall@gmail.com>
2022-03-18 17:43:25 +0000

    works but needs env var for PUBLICATION_SERVICES_URL

--- src/test/java/uk/gov/hmcts/reform/pip/account/management/service/NotifyEmailServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/account/management/service/NotifyEmailServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.pip.account.management.Application;
 
 import java.io.IOException;
