cde201a1baa3292bbc607efa60560e7e444bcbf7
Daniel Furnivall <danielfurnivall@gmail.com>
2022-04-11 09:50:41 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/pip/account/management/service/PublicationService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/service/PublicationService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientException;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
