aea5fe89eaa1d9662e10822235eeba5c9c6104f9
KianKwa <kian.kwa@justice.gov.uk>
2022-07-13 09:25:31 +0100

    Checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/pip/account/management/model/PiUser.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/model/PiUser.java
@@ -11,7 +11,14 @@ import uk.gov.hmcts.reform.pip.account.management.validation.annotations.ValidPr
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
