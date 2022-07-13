f96e8ef7378ff255bbdd6916cc06717a5dd497fc
KianKwa <kian.kwa@justice.gov.uk>
2022-07-13 09:14:57 +0100

    Add creation date to account user table

--- src/main/java/uk/gov/hmcts/reform/pip/account/management/model/PiUser.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/model/PiUser.java
@@ -1,19 +1,17 @@
 package uk.gov.hmcts.reform.pip.account.management.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import uk.gov.hmcts.reform.pip.account.management.validation.annotations.ValidProvenanceUserId;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -26,6 +24,7 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 @ValidProvenanceUserId
+@EntityListeners(AuditingEntityListener.class)
 public class PiUser {
 
     /**
@@ -61,4 +60,8 @@ public class PiUser {
      */
     @Enumerated(EnumType.STRING)
     private Roles roles;
+
+    @CreatedDate
+    @ApiModelProperty(hidden = true)
+    private LocalDateTime createdDate;
 }
