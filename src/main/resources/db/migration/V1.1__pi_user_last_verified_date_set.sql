342e0c157d187cd77713b49c9df6427a46f35289
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-08-03 13:41:06 +0100

    Update flyway and script

--- src/main/resources/db/migration/V1.1__pi_user_last_verified_date_set.sql
+++ src/main/resources/db/migration/V1.1__pi_user_last_verified_date_set.sql
@@ -1,8 +1,26 @@
---  This script sets the last_verified_date as either the
---  created date if it is not null or
---  the current timestamp when the script is run
+--
+-- Create the table if doesn't exist for test DBs
+--
+CREATE TABLE IF NOT EXISTS pi_user (
+    user_id uuid NOT NULL PRIMARY KEY,
+    email varchar(255),
+    provenance_user_id varchar(255) NOT NULL,
+    roles varchar(255),
+    user_provenance varchar(255),
+    created_date timestamp,
+    last_verified_date timestamp
+  );
+
+--
+-- Set the created date to 12th July 2pm for any users that do not have a created date
+--
 UPDATE pi_user
-SET last_verified_date =
-      CASE WHEN created_date IS NOT NULL THEN created_date
-           ELSE CURRENT_TIMESTAMP
-        END;
+SET created_date = '2022-07-12 14:00:00'
+WHERE created_date IS NULL;
+
+--
+-- Sets the last_verified_date to the created_date for verified users
+--
+UPDATE pi_user
+SET last_verified_date = created_date
+WHERE roles = 'VERIFIED';
