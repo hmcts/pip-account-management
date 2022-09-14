7fe201d385207f09e4be7b9bd4fba59ad6e32bf3
KianKwa <kian.kwa@justice.gov.uk>
2022-09-14 09:10:06 +0100

    Add scheduling endpoints for AAD admin and IDAM inactive users

--- src/functionalTest/java/uk/gov/hmcts/reform/pip/account/management/controllers/AccountTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pip/account/management/controllers/AccountTest.java
@@ -117,6 +117,10 @@ class AccountTest {
     private static final String CREATE_MEDIA_USER_URL = "/application";
     private static final String GET_PROVENANCE_USER_URL = ROOT_URL + "/provenance/";
     private static final String UPDATE_ACCOUNT_URL = ROOT_URL + "/provenance/";
+    private static final String NOTIFY_INACTIVE_ADMIN_ACCOUNTS_URL = ROOT_URL + "/admin/inactive/notify";
+    private static final String DELETE_EXPIRED_ADMIN_ACCOUNTS_URL = ROOT_URL + "/admin/inactive";
+    private static final String NOTIFY_INACTIVE_IDAM_ACCOUNTS_URL = ROOT_URL + "/idam/inactive/notify";
+    private static final String DELETE_EXPIRED_IDAM_ACCOUNTS_URL = ROOT_URL + "/idam/inactive";
     private static final String EMAIL_URL = ROOT_URL + "/emails";
     private static final String EMAIL = "test_account_admin@hmcts.net";
     private static final String INVALID_EMAIL = "ab";
@@ -1044,5 +1048,37 @@ class AccountTest {
         mockMvc.perform(mockHttpServletRequestBuilder)
             .andExpect(status().isBadRequest())
             .andExpect(content().string(containsString("The field 'email' could not be updated")));
+    }
+
+    @Test
+    void testNotifyInactiveAdminAccountsSuccess() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+            .post(NOTIFY_INACTIVE_ADMIN_ACCOUNTS_URL);
+
+        mockMvc.perform(request).andExpect(status().isNoContent());
+    }
+
+    @Test
+    void testDeleteExpiredAdminAccountsSuccess() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+            .delete(DELETE_EXPIRED_ADMIN_ACCOUNTS_URL);
+
+        mockMvc.perform(request).andExpect(status().isNoContent());
+    }
+
+    @Test
+    void testNotifyInactiveIdamAccountsSuccess() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+            .post(NOTIFY_INACTIVE_IDAM_ACCOUNTS_URL);
+
+        mockMvc.perform(request).andExpect(status().isNoContent());
+    }
+
+    @Test
+    void testDeleteExpiredIdamAccountsSuccess() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+            .delete(DELETE_EXPIRED_IDAM_ACCOUNTS_URL);
+
+        mockMvc.perform(request).andExpect(status().isNoContent());
     }
 }
--- src/test/java/uk/gov/hmcts/reform/pip/account/management/service/AccountVerificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/account/management/service/AccountVerificationServiceTest.java
@@ -19,6 +19,7 @@ import uk.gov.hmcts.reform.pip.account.management.model.UserProvenances;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,6 +31,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
+@SuppressWarnings("PMD.TooManyMethods")
 class AccountVerificationServiceTest {
     private static final String MEDIA_USER_EMAIL = "media@test.com";
     private static final String AAD_ADMIN_USER_EMAIL = "aad_admin@test.com";
@@ -225,5 +227,125 @@ class AccountVerificationServiceTest {
 
         verifyNoInteractions(accountService);
         verifyNoInteractions(publicationService);
+    }
+
+    @Test
+    void testSendMediaUsersForVerification() throws AzureCustomException {
+        when(userRepository.findVerifiedUsersByLastVerifiedDate(anyInt()))
+            .thenReturn(Collections.singletonList(MEDIA_USER));
+        when(azureUserService.getUser(MEDIA_USER_EMAIL)).thenReturn(azureMediaUser);
+
+        accountVerificationService.sendMediaUsersForVerification();
+        verify(publicationService).sendAccountVerificationEmail(MEDIA_USER_EMAIL, AZURE_MEDIA_USER_NAME);
+    }
+
+    @Test
+    void testNoMediaUsersForVerification() {
+        when(userRepository.findVerifiedUsersByLastVerifiedDate(anyInt()))
+            .thenReturn(Collections.emptyList());
+
+        accountVerificationService.sendMediaUsersForVerification();
+        verifyNoInteractions(publicationService);
+        verifyNoInteractions(azureUserService);
+    }
+
+    @Test
+    void testNotifyAdminUsersToSignIn() throws AzureCustomException {
+        when(userRepository.findAadAdminUsersByLastSignedInDate(anyInt()))
+            .thenReturn(Collections.singletonList(AAD_ADMIN_USER));
+        when(azureUserService.getUser(AAD_ADMIN_USER_EMAIL)).thenReturn(azureAdminUser);
+
+        accountVerificationService.notifyAdminUsersToSignIn();
+        verify(publicationService).sendInactiveAccountSignInNotificationEmail(
+            AAD_ADMIN_USER_EMAIL, AZURE_ADMIN_USER_NAME, LAST_SIGNED_IN_DATE_STRING
+        );
+    }
+
+    @Test
+    void testNoNotificationOfAdminUsersToSignIn() {
+        when(userRepository.findAadAdminUsersByLastSignedInDate(anyInt()))
+            .thenReturn(Collections.emptyList());
+
+        accountVerificationService.notifyAdminUsersToSignIn();
+        verifyNoInteractions(publicationService);
+        verifyNoInteractions(azureUserService);
+    }
+
+    @Test
+    void testNotifyIdamUsersToSignIn() {
+        when(userRepository.findIdamUsersByLastSignedInDate(anyInt()))
+            .thenReturn(List.of(CFT_IDAM_USER, CRIME_IDAM_USER));
+
+        accountVerificationService.notifyIdamUsersToSignIn();
+        verify(publicationService).sendInactiveAccountSignInNotificationEmail(
+            CFT_IDAM_USER_EMAIL, IDAM_USER_NAME, LAST_SIGNED_IN_DATE_STRING
+        );
+        verify(publicationService).sendInactiveAccountSignInNotificationEmail(
+            CRIME_IDAM_USER_EMAIL, IDAM_USER_NAME, LAST_SIGNED_IN_DATE_STRING
+        );
+    }
+
+    @Test
+    void testNoNotificationOfIdamUsersToSignIn() {
+        when(userRepository.findIdamUsersByLastSignedInDate(anyInt()))
+            .thenReturn(Collections.emptyList());
+
+        accountVerificationService.notifyIdamUsersToSignIn();
+        verifyNoInteractions(publicationService);
+    }
+
+    @Test
+    void testMediaAccountDeletion() {
+        when(userRepository.findVerifiedUsersByLastVerifiedDate(anyInt()))
+            .thenReturn(Collections.singletonList(MEDIA_USER));
+
+        accountVerificationService.findMediaAccountsForDeletion();
+        verify(accountService).deleteAccount(MEDIA_USER_EMAIL, true);
+    }
+
+    @Test
+    void testNoMediaAccountDeletion() {
+        when(userRepository.findVerifiedUsersByLastVerifiedDate(anyInt()))
+            .thenReturn(Collections.emptyList());
+
+        accountVerificationService.findMediaAccountsForDeletion();
+        verifyNoInteractions(accountService);
+    }
+
+    @Test
+    void testAdminAccountDeletion() {
+        when(userRepository.findAadAdminUsersByLastSignedInDate(anyInt()))
+            .thenReturn(Collections.singletonList(AAD_ADMIN_USER));
+
+        accountVerificationService.findAdminAccountsForDeletion();
+        verify(accountService).deleteAccount(AAD_ADMIN_USER_EMAIL, true);
+    }
+
+    @Test
+    void testNoAdminAccountDeletion() {
+        when(userRepository.findAadAdminUsersByLastSignedInDate(anyInt()))
+            .thenReturn(Collections.emptyList());
+
+        accountVerificationService.findAdminAccountsForDeletion();
+        verifyNoInteractions(accountService);
+    }
+
+    @Test
+    void testIdamAccountDeletion() {
+        when(userRepository.findIdamUsersByLastSignedInDate(anyInt()))
+            .thenReturn(List.of(CFT_IDAM_USER, CRIME_IDAM_USER));
+
+        accountVerificationService.findIdamAccountsForDeletion();
+        verify(accountService).deleteAccount(CFT_IDAM_USER_EMAIL, false);
+        verify(accountService).deleteAccount(CRIME_IDAM_USER_EMAIL, false);
+    }
+
+    @Test
+    void testNoIdamAccountDeletion() {
+        when(userRepository.findIdamUsersByLastSignedInDate(anyInt()))
+            .thenReturn(Collections.emptyList());
+
+        accountVerificationService.findIdamAccountsForDeletion();
+        verifyNoInteractions(accountService);
     }
 }
