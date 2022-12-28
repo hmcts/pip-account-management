fa88d12efd921c67ecfb1084d5bdf56e6c8bb047
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2022-12-28 12:07:37 +0000

    fix code smell

--- src/main/java/uk/gov/hmcts/reform/pip/account/management/errorhandling/exceptions/SystemAdminAccountException.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/errorhandling/exceptions/SystemAdminAccountException.java
@@ -11,7 +11,7 @@ public class SystemAdminAccountException extends RuntimeException {
 
     private static final long serialVersionUID = 5618103537116781009L;
 
-    private final ErroredSystemAdminAccount erroredSystemAdminAccount;
+    private final transient ErroredSystemAdminAccount erroredSystemAdminAccount;
 
 
     public SystemAdminAccountException(ErroredSystemAdminAccount erroredSystemAdminAccount) {
