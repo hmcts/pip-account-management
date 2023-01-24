c76fb028f1c0dd26b49e7a92946012d82fc7f6fb
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-01-24 14:55:41 +0000

    Updated enum with audit actions

--- src/main/java/uk/gov/hmcts/reform/pip/account/management/model/AuditAction.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/model/AuditAction.java
@@ -4,6 +4,15 @@ package uk.gov.hmcts.reform.pip.account.management.model;
  * Enum that contains the different actions that are audited.
  */
 public enum AuditAction {
-    USER_DELETE,
-    // TODO
+    ATTEMPT_SYSTEM_ADMIN_CREATION,
+    REFERENCE_DATA_UPLOAD,
+    REFERENCE_DATA_DOWNLOAD,
+    MANAGE_THIRD_PARTY,
+    MANAGE_THIRD_PARTY_SUBSCRIPTIONS,
+    USER_MANAGEMENT_VIEW,
+    MANAGE_USER,
+    UPDATE_USER,
+    DELETE_USER,
+    VIEW_BLOB_EXPLORER,
+    BULK_MEDIA_UPLOAD
 }
