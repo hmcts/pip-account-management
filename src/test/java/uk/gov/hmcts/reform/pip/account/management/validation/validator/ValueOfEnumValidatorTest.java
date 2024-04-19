857573d3adaf31e8681711b20c15c0be95d5dddb
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-04-19 09:13:54 +0100

    PUB-2117 - Fixed PMD

--- src/test/java/uk/gov/hmcts/reform/pip/account/management/validation/validator/ValueOfEnumValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/account/management/validation/validator/ValueOfEnumValidatorTest.java
@@ -3,10 +3,14 @@ package uk.gov.hmcts.reform.pip.account.management.validation.validator;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.pip.account.management.model.MediaApplication;
+import uk.gov.hmcts.reform.pip.account.management.model.MediaApplicationStatus;
 
 import java.util.Set;
 
@@ -15,17 +19,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ExtendWith(MockitoExtension.class)
 class ValueOfEnumValidatorTest {
 
-    @Test
-    void testNullStatusValue() {
-        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-        Validator validator = validatorFactory.getValidator();
-        MediaApplication mediaApplication = new MediaApplication();
+    private Validator validator;
+    private ValidatorFactory validatorFactory;
+    private final MediaApplication mediaApplication = new MediaApplication();
+
+    @BeforeEach
+    void setup() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
 
         mediaApplication.setFullName("Test user");
         mediaApplication.setEmail("test@justice.gov.uk");
         mediaApplication.setEmployer("MoJ");
+    }
 
-        mediaApplication.setStatus(null);
+    @AfterEach
+    void rundown() {
+        validatorFactory.close();
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void testInvalidValues(MediaApplicationStatus statusValue) {
+        mediaApplication.setStatus(statusValue);
         Set violations = validator.validate(mediaApplication);
 
         assertTrue(!violations.isEmpty(), "A value that is invalid was marked as valid");
