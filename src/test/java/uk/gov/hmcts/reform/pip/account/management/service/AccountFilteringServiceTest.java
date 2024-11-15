6460f7a49b82c53b423acd53512ce6ca5d1b03cb
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-11-15 09:09:49 +0000

    Update tests

--- src/test/java/uk/gov/hmcts/reform/pip/account/management/service/AccountFilteringServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/account/management/service/AccountFilteringServiceTest.java
@@ -11,17 +11,18 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import uk.gov.hmcts.reform.pip.account.management.database.UserRepository;
+import uk.gov.hmcts.reform.pip.account.management.dto.MiReportData;
 import uk.gov.hmcts.reform.pip.account.management.errorhandling.exceptions.NotFoundException;
 import uk.gov.hmcts.reform.pip.account.management.model.PiUser;
 import uk.gov.hmcts.reform.pip.model.account.Roles;
 import uk.gov.hmcts.reform.pip.model.account.UserProvenances;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -31,6 +32,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.pip.model.account.Roles.ALL_NON_RESTRICTED_ADMIN_ROLES;
+import static uk.gov.hmcts.reform.pip.model.account.Roles.INTERNAL_ADMIN_CTSC;
 import static uk.gov.hmcts.reform.pip.model.account.UserProvenances.PI_AAD;
 import static uk.gov.hmcts.reform.pip.model.account.UserProvenances.SSO;
 
@@ -38,11 +40,10 @@ import static uk.gov.hmcts.reform.pip.model.account.UserProvenances.SSO;
 class AccountFilteringServiceTest {
     private static final String EMAIL = "test@hmcts.net";
     private static final String ID = "1234";
-    private static final List<String> EXAMPLE_CSV = List.of(
-        "2fe899ff-96ed-435a-bcad-1411bbe96d2a,string,CFT_IDAM,INTERNAL_ADMIN_CTSC,2022-01-19 13:45:50.873778,"
-            + "2023-01-25 14:22:56.434343");
+    private static final UUID USER_ID = UUID.randomUUID();
+    private static final LocalDateTime CREATED_DATE = LocalDateTime.of(2022,1, 19, 13, 45, 50);
+    private static final LocalDateTime LAST_SIGNED_IN = LocalDateTime.of(2023,1, 25, 14, 22, 43);
     private static final PiUser PI_USER = new PiUser();
-
     private static final String USER_NOT_FOUND_EXCEPTION_MESSAGE =
         "The exception when a user has not been found has been thrown";
     private static final String RETURN_USER_ERROR = "Returned user does not match expected user";
@@ -63,26 +64,14 @@ class AccountFilteringServiceTest {
 
     @Test
     void testMiService() {
-        when(userRepository.getAccManDataForMI()).thenReturn(EXAMPLE_CSV);
-        String testString = accountFilteringService.getAccManDataForMiReporting();
+        MiReportData miRecord = new MiReportData(USER_ID, ID, PI_AAD, INTERNAL_ADMIN_CTSC,
+                                                                       CREATED_DATE, LAST_SIGNED_IN);
+        List<MiReportData> miData = List.of(miRecord, miRecord);
 
-        assertThat(testString)
-            .as("Json parsing has probably failed")
-            .contains("CTSC")
-            .hasLineCount(2);
+        when(userRepository.getAccManDataForMI()).thenReturn(miData);
+        List<MiReportData> miReportData = accountFilteringService.getAccManDataForMiReporting();
 
-        String[] splitLineString = testString.split("(\r\n|\r|\n)");
-        long countLine1 = splitLineString[0].chars().filter(character -> character == ',').count();
-
-        assertThat(splitLineString[0])
-            .as("Header row does not match")
-            .isEqualTo("user_id,provenance_user_id,user_provenance,roles,created_date,last_signed_in_date");
-
-        assertThat(splitLineString)
-            .as("Data must be missing, are only headers printing? or Wrong data")
-            .hasSizeGreaterThanOrEqualTo(2)
-            .allSatisfy(
-                e -> assertThat(e.chars().filter(character -> character == ',').count()).isEqualTo(countLine1));
+        assertEquals(miData, miReportData, "Returned data does not match expected data");
     }
 
     @Test
