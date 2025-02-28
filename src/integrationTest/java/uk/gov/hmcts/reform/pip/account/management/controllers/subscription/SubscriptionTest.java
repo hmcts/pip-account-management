d4436c08fa78a188d426c8c8d5ce675a5256f5e6
Kian Kwa <kian.kwa@justice.gov.uk>
2025-02-28 12:24:46 +0000

    Merge Removal of Subscription Mi Data changes into this branch

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/controllers/subscription/SubscriptionTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/controllers/subscription/SubscriptionTest.java
@@ -106,8 +106,6 @@ class SubscriptionTest extends IntegrationTestBase {
 
     private static final String SUBSCRIPTION_BASE_URL = "/subscription/";
     private static final String SUBSCRIPTION_PATH = "/subscription";
-    private static final String MI_REPORTING_SUBSCRIPTION_DATA_ALL_URL = "/subscription/mi-data-all";
-    private static final String MI_REPORTING_SUBSCRIPTION_DATA_LOCAL_URL = "/subscription/mi-data-local";
     private static final String MI_REPORTING_SUBSCRIPTION_DATA_ALL_URL_V2 = "/subscription/v2/mi-data-all";
     private static final String MI_REPORTING_SUBSCRIPTION_DATA_LOCATION_URL_V2 = "/subscription/v2/mi-data-location";
     private static final String SUBSCRIPTION_USER_PATH = "/subscription/user/" + UUID_STRING;
@@ -122,9 +120,6 @@ class SubscriptionTest extends IntegrationTestBase {
     private static final String OPENING_BRACKET = "[\"";
     private static final String CLOSING_BRACKET = "\"]";
     private static final String DOUBLE_QUOTE_COMMA = "\",\"";
-    private static final String EXPECTED_MI_DATA_ALL_HEADERS = "id,channel,search_type,user_id,court_name,created_date";
-    private static final String EXPECTED_MI_DATA_LOCAL_HEADERS = "id,search_value,channel,user_id,court_name,"
-        + "created_date";
 
     private static final String ACTIONING_USER_ID = "87f907d2-eb28-42cc-b6e1-ae2b03f7bba5";
     private static final String INVALID_ACTIONING_USER_ID = "87f907d2-eb28-42cc-b6e1-ae2b03f7bba6";
@@ -904,52 +899,6 @@ class SubscriptionTest extends IntegrationTestBase {
         MvcResult mvcResult = mvc.perform(request).andExpect(status().isForbidden()).andReturn();
 
         assertEquals(FORBIDDEN.value(), mvcResult.getResponse().getStatus(),
-                     FORBIDDEN_STATUS_CODE);
-    }
-
-    @Test
-    void testGetSubscriptionDataForMiReportingAll() throws Exception {
-        mvc.perform(setupMockSubscription(CASE_ID, SearchType.CASE_ID, VALID_USER_ID))
-            .andExpect(status().isCreated());
-        String response = mvc.perform(get(MI_REPORTING_SUBSCRIPTION_DATA_ALL_URL))
-            .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
-
-        assertEquals(EXPECTED_MI_DATA_ALL_HEADERS, response.split("\n")[0],
-                     "Should successfully retrieve MI data headers");
-        assertThat(response.contains(VALID_USER_ID));
-    }
-
-    @Test
-    @WithMockUser(username = UNAUTHORIZED_USERNAME, authorities = {UNAUTHORIZED_ROLE})
-    void testGetSubscriptionDataForMiReportingAllUnauthorized() throws Exception {
-        MvcResult response = mvc.perform(get(MI_REPORTING_SUBSCRIPTION_DATA_ALL_URL))
-            .andExpect(status().isForbidden())
-            .andReturn();
-
-        assertEquals(FORBIDDEN.value(), response.getResponse().getStatus(),
-                     FORBIDDEN_STATUS_CODE);
-    }
-
-    @Test
-    void testGetSubscriptionDataForMiReportingLocal() throws Exception {
-        mvc.perform(setupMockSubscription(LOCATION_ID, SearchType.LOCATION_ID, VALID_USER_ID))
-            .andExpect(status().isCreated());
-        String response = mvc.perform(get(MI_REPORTING_SUBSCRIPTION_DATA_LOCAL_URL))
-            .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
-
-        assertEquals(EXPECTED_MI_DATA_LOCAL_HEADERS, response.split("\n")[0],
-                     "Should successfully retrieve MI data headers");
-        assertThat(response.contains(VALID_USER_ID));
-    }
-
-    @Test
-    @WithMockUser(username = UNAUTHORIZED_USERNAME, authorities = {UNAUTHORIZED_ROLE})
-    void testGetSubscriptionDataForMiReportingLocalUnauthorized() throws Exception {
-        MvcResult response = mvc.perform(get(MI_REPORTING_SUBSCRIPTION_DATA_LOCAL_URL))
-            .andExpect(status().isForbidden())
-            .andReturn();
-
-        assertEquals(FORBIDDEN.value(), response.getResponse().getStatus(),
                      FORBIDDEN_STATUS_CODE);
     }
 
--- src/main/java/uk/gov/hmcts/reform/pip/account/management/service/subscription/SubscriptionService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/service/subscription/SubscriptionService.java
@@ -23,7 +23,6 @@ import static uk.gov.hmcts.reform.pip.model.subscription.SearchType.LOCATION_ID;
  */
 @Slf4j
 @Service
-@SuppressWarnings("PMD.TooManyMethods")
 public class SubscriptionService {
 
     private final SubscriptionRepository subscriptionRepository;
@@ -126,34 +125,8 @@ public class SubscriptionService {
         });
     }
 
-    /**
-     * Previous version of the MI Reporting service method. No longer used and soon to be removed.
-     * @deprecated This method will be removed in the future in favour of the V2 equivalent.
-     */
-    @Deprecated(since = "2")
-    public String getAllSubscriptionsDataForMiReporting() {
-        StringBuilder builder = new StringBuilder(60);
-        builder.append("id,channel,search_type,user_id,court_name,created_date").append(System.lineSeparator());
-        subscriptionRepository.getAllSubsDataForMi()
-            .forEach(line -> builder.append(line).append(System.lineSeparator()));
-        return builder.toString();
-    }
-
     public List<AllSubscriptionMiData> getAllSubscriptionsDataForMiReportingV2() {
         return subscriptionRepository.getAllSubsDataForMiV2();
-    }
-
-    /**
-     * Previous version of the MI Reporting service method. No longer used and soon to be removed.
-     * @deprecated This method will be removed in the future in favour of the V2 equivalent.
-     */
-    @Deprecated(since = "2")
-    public String getLocalSubscriptionsDataForMiReporting() {
-        StringBuilder builder = new StringBuilder(60);
-        builder.append("id,search_value,channel,user_id,court_name,created_date").append(System.lineSeparator());
-        subscriptionRepository.getLocalSubsDataForMi()
-            .forEach(line -> builder.append(line).append(System.lineSeparator()));
-        return builder.toString();
     }
 
     public List<LocationSubscriptionMiData> getLocationSubscriptionsDataForMiReportingV2() {
