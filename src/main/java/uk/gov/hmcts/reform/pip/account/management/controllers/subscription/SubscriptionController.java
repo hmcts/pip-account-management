eb4c3077965e220021af551d2cb5ffebed5654bd
Kian Kwa <kian.kwa@justice.gov.uk>
2025-02-28 12:29:16 +0000

    Fixed PMD

--- src/main/java/uk/gov/hmcts/reform/pip/account/management/controllers/subscription/SubscriptionController.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/controllers/subscription/SubscriptionController.java
@@ -2,16 +2,12 @@ package uk.gov.hmcts.reform.pip.account.management.controllers.subscription;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
