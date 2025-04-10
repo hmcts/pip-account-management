de9c92faeec8dbfe8cc1a05ae835d5bd2f1cc3ac
Natasha Alker <NatashaAlker@yahoo.co.uk>
2025-04-10 16:15:49 +0100

    Restrict view filtered accounts endpoints for system admin role only

--- src/main/java/uk/gov/hmcts/reform/pip/account/management/controllers/AccountFilteringController.java
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/controllers/AccountFilteringController.java
@@ -4,15 +4,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,20 +35,19 @@ import java.util.List;
 @ApiResponse(responseCode = "403", description = "User has not been authorized")
 @Validated
 @IsAdmin
+@AllArgsConstructor
 @SecurityRequirement(name = "bearerAuth")
 @SuppressWarnings("squid:S1133")
 public class AccountFilteringController {
+    private static final String REQUESTER_ID = "x-requester-id";
+
     private static final String OK_CODE = "200";
+    private static final String FORBIDDEN_ERROR_CODE = "401";
     private static final String NOT_FOUND_ERROR_CODE = "404";
 
     private static final String PI_USER = "{piUser}";
 
     private final AccountFilteringService accountFilteringService;
-
-    @Autowired
-    public AccountFilteringController(AccountFilteringService accountFilteringService) {
-        this.accountFilteringService = accountFilteringService;
-    }
 
     @ApiResponse(responseCode = OK_CODE, description = "List of Accounts MI Data")
     @Operation(summary = "Returns anonymized account data for MI reporting")
@@ -56,16 +57,21 @@ public class AccountFilteringController {
     }
 
     @ApiResponse(responseCode = OK_CODE, description = "List of third party accounts")
+    @ApiResponse(responseCode = FORBIDDEN_ERROR_CODE, description = "Action forbidden")
     @Operation(summary = "Get all third party accounts")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/all/third-party")
-    public ResponseEntity<List<PiUser>> getAllThirdPartyAccounts() {
+    @PreAuthorize("@authorisationService.userCanViewAccountDetails(#requesterId)")
+    public ResponseEntity<List<PiUser>> getAllThirdPartyAccounts(@RequestHeader(REQUESTER_ID) String requesterId) {
         return ResponseEntity.ok(accountFilteringService.findAllThirdPartyAccounts());
     }
 
     @Operation(summary = "Get all accounts except third party in a page with filtering")
+    @ApiResponse(responseCode = FORBIDDEN_ERROR_CODE, description = "Action forbidden")
     @GetMapping("/all")
+    @PreAuthorize("@authorisationService.userCanViewAccountDetails(#requesterId)")
     public ResponseEntity<Page<PiUser>> getAllAccountsExceptThirdParty(
+        @RequestHeader(REQUESTER_ID) String requesterId,
         @RequestParam(name = "pageNumber", defaultValue = "0") int pageNumber,
         @RequestParam(name = "pageSize", defaultValue = "25") int pageSize,
         @RequestParam(name = "email", defaultValue = "", required = false) String email,
