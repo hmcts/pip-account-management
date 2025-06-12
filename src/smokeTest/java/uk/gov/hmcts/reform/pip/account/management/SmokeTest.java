cfd2df67bb09cdb3f3eb8910d0abdb5ab3a657cc
Natasha Alker <NatashaAlker@yahoo.co.uk>
2025-06-12 13:17:25 +0100

    Update smoke test

--- src/smokeTest/java/uk/gov/hmcts/reform/pip/account/management/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/pip/account/management/SmokeTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ActiveProfiles;
