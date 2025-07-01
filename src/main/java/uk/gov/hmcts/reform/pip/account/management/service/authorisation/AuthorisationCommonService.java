ab1000e8d78128b82b58905f3781548bb9c5adec
Natasha Alker <NatashaAlker@yahoo.co.uk>
2025-07-01 11:31:19 +0100

    Separate common authorisation methods

---
+++ src/main/java/uk/gov/hmcts/reform/pip/account/management/service/authorisation/AuthorisationCommonService.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.pip.account.management.service.authorisation;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.pip.account.management.model.account.PiUser;
+import uk.gov.hmcts.reform.pip.account.management.service.account.AccountService;
+
+import java.util.UUID;
+
+import static uk.gov.hmcts.reform.pip.model.account.Roles.SYSTEM_ADMIN;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AuthorisationCommonService {
+
+    private final AccountService accountService;
+
+    public boolean isAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return hasAuthority(authentication, "APPROLE_api.request.admin");
+    }
+
+    public boolean hasAuthority(Authentication authentication, String role) {
+        return authentication.getAuthorities().stream()
+            .anyMatch(granted -> granted.getAuthority().equals(role));
+    }
+
+    public boolean isSystemAdmin(UUID userId) {
+        PiUser user = accountService.getUserById(userId);
+        return user != null && user.getRoles() == SYSTEM_ADMIN;
+    }
+}
