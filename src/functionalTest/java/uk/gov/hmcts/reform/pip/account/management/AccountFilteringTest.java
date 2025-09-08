2f98dbc4eacf58ce63d7b328b7db73f152daa7b4
kian.kwa <kian.kwa@justice.gov.uk>
2025-09-08 09:26:41 +0100

    Investigate running of nightly functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/pip/account/management/AccountFilteringTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pip/account/management/AccountFilteringTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.Page;
 import uk.gov.hmcts.reform.pip.account.management.utils.AccountHelperBase;
@@ -71,6 +72,7 @@ class AccountFilteringTest extends AccountHelperBase {
     }
 
     @Test
+    @Tag("Nightly")
     void testGetAllThirdPartyAccounts() {
         Response getThirdParties = doGetRequest(GET_ALL_THIRD_PARTY_ACCOUNTS, bearer);
 
@@ -83,6 +85,7 @@ class AccountFilteringTest extends AccountHelperBase {
     }
 
     @Test
+    @Tag("Nightly")
     void testGetAllAccountsExceptThirdPartyDoesNotContainThirdParty() {
         Map<String, String> requestParams = new ConcurrentHashMap<>();
         requestParams.put("provenances", UserProvenances.THIRD_PARTY.toString());
