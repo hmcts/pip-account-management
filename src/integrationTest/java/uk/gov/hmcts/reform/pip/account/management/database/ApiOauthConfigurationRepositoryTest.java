4b2e4fccae86355b9140d4b2cf440ce2a24ecb02
kian.kwa <kian.kwa@justice.gov.uk>
2025-12-30 15:24:02 +0000

    Update repository tests

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/database/ApiOauthConfigurationRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/database/ApiOauthConfigurationRepositoryTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.pip.account.management.database;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.pip.account.management.model.thirdparty.ApiOauthConfiguration;
+import uk.gov.hmcts.reform.pip.account.management.model.thirdparty.ApiUser;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -19,29 +21,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ApiOauthConfigurationRepositoryTest {
-    private static final UUID USER_ID = UUID.randomUUID();
     private static final UUID INVALID_USER_ID = UUID.randomUUID();
-    private static final UUID CONFIG_ID = UUID.randomUUID();
     private static final String CLIENT_ID_KEY = "TestKey";
+    private static final String USER_NAME = "Test name";
+
+    private UUID userId;
 
     @Autowired
     ApiOauthConfigurationRepository apiOauthConfigurationRepository;
 
+    @Autowired
+    ApiUserRepository apiUserRepository;
+
+    @BeforeAll
+    void setup() {
+        ApiUser apiUser = new ApiUser();
+        apiUser.setName(USER_NAME);
+        ApiUser createdApiUser = apiUserRepository.save(apiUser);
+        userId = createdApiUser.getUserId();
+
+        ApiOauthConfiguration apiOauthConfiguration = new ApiOauthConfiguration();
+        apiOauthConfiguration.setUserId(userId);
+        apiOauthConfiguration.setClientIdKey(CLIENT_ID_KEY);
+
+        apiOauthConfigurationRepository.save(apiOauthConfiguration);
+    }
+
     @AfterAll
     void shutdown() {
         apiOauthConfigurationRepository.deleteAll();
+        apiUserRepository.deleteAll();
     }
 
     @Test
     void shouldFindApiOauthConfigurationByUserId() {
-        ApiOauthConfiguration apiOauthConfiguration = new ApiOauthConfiguration();
-        apiOauthConfiguration.setId(CONFIG_ID);
-        apiOauthConfiguration.setUserId(USER_ID);
-        apiOauthConfiguration.setClientIdKey(CLIENT_ID_KEY);
-        apiOauthConfigurationRepository.save(apiOauthConfiguration);
-
         Optional<ApiOauthConfiguration> foundApiOauthConfiguration = apiOauthConfigurationRepository
-            .findByUserId(USER_ID);
+            .findByUserId(userId);
 
         assertThat(foundApiOauthConfiguration)
             .as("Third-party API OAuth configuration should be found")
@@ -50,15 +65,15 @@ class ApiOauthConfigurationRepositoryTest {
             .extracting(ApiOauthConfiguration::getClientIdKey)
             .isEqualTo(CLIENT_ID_KEY);
 
-        apiOauthConfigurationRepository.deleteByUserId(USER_ID);
+        apiOauthConfigurationRepository.deleteByUserId(userId);
 
-        assertThat(apiOauthConfigurationRepository.findByUserId(USER_ID))
+        assertThat(apiOauthConfigurationRepository.findByUserId(userId))
             .as("Third-party API OAuth configuration should be deleted")
             .isNotPresent();
     }
 
     @Test
-    void shouldNotFindApiOauthConfigurationByINvalidUserId() {
+    void shouldNotFindApiOauthConfigurationByInvalidUserId() {
         assertThat(apiOauthConfigurationRepository.findByUserId(INVALID_USER_ID))
             .as("Third-party API OAuth configuration should not exist")
             .isNotPresent();
