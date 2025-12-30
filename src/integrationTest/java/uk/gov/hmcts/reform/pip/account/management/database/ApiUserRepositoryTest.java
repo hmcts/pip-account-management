f5ee8985ed68ba2345a3d882be2211c7e5288012
kian.kwa <kian.kwa@justice.gov.uk>
2025-12-30 12:27:14 +0000

    Make API user repository tests independent

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/database/ApiUserRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/database/ApiUserRepositoryTest.java
@@ -1,14 +1,16 @@
 package uk.gov.hmcts.reform.pip.account.management.database;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.pip.account.management.model.thirdparty.ApiUser;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,19 +19,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("integration-jpa")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ApiUserRepositoryTest {
-    private static final UUID USER_ID = UUID.randomUUID();
+    private static final UUID USER_ID1 = UUID.randomUUID();
+    private static final UUID USER_ID2 = UUID.randomUUID();
     private static final String USER_NAME = "Test name";
 
     @Autowired
     ApiUserRepository apiUserRepository;
 
-    @BeforeEach
+    @BeforeAll
     void setup() {
-        ApiUser apiUser = new ApiUser();
-        apiUser.setUserId(USER_ID);
-        apiUser.setName(USER_NAME);
-        apiUserRepository.save(apiUser);
+        ApiUser apiUser1 = new ApiUser();
+        apiUser1.setUserId(USER_ID1);
+        apiUser1.setName(USER_NAME);
+
+        ApiUser apiUser2 = new ApiUser();
+        apiUser2.setUserId(USER_ID1);
+        apiUser2.setName(USER_NAME);
+
+        apiUserRepository.saveAll(List.of(apiUser1, apiUser2));
     }
 
     @AfterEach
@@ -39,7 +48,7 @@ class ApiUserRepositoryTest {
 
     @Test
     void shouldFindApiUserByUserId() {
-        Optional<ApiUser> apiUser = apiUserRepository.findByUserId(USER_ID);
+        Optional<ApiUser> apiUser = apiUserRepository.findByUserId(USER_ID1);
 
         assertThat(apiUser)
             .as("Third-party API user does not match")
@@ -51,9 +60,13 @@ class ApiUserRepositoryTest {
 
     @Test
     void shouldDeleteApiUserByUserId() {
-        apiUserRepository.deleteByUserId(USER_ID);
+        assertThat(apiUserRepository.findByUserId(USER_ID2))
+            .as("Third-party API user exists")
+            .isPresent();
 
-        assertThat(apiUserRepository.findByUserId(USER_ID))
+        apiUserRepository.deleteByUserId(USER_ID2);
+
+        assertThat(apiUserRepository.findByUserId(USER_ID1))
             .as("Third-party API user should be deleted")
             .isNotPresent();
     }
