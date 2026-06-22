be951e80eac7b3825f68987cc5b2a6bb93c05c82
KianKwa <kian.kwa@justice.gov.uk>
2026-06-22 18:18:20 +0100

    Update flyway script

--- src/main/resources/db/migration/V1.16__copy_case_id_and_urn_subscription.sql
+++ src/main/resources/db/migration/V1.16__copy_case_id_and_urn_subscription.sql
@@ -1,4 +1,4 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Extend the search_type check constraint to include the new CASE_NUMBER and CASE_NAME values
 ALTER TABLE subscription
@@ -10,7 +10,7 @@ DROP CONSTRAINT IF EXISTS subscription_search_type_check,
 -- Copy CASE_ID subscriptions to new CASE_NUMBER rows
 INSERT INTO subscription (id, user_id, search_type, search_value, channel, created_date, case_number, case_name,
                           last_updated_date)
-SELECT gen_random_uuid(),
+SELECT uuid_generate_v4(),
        user_id,
        'CASE_NUMBER',
        search_value,
@@ -25,7 +25,7 @@ WHERE search_type = 'CASE_ID';
 -- Copy CASE_URN subscriptions to new CASE_NUMBER rows, mapping the urn field to case_number
 INSERT INTO subscription (id, user_id, search_type, search_value, channel, created_date, case_number, case_name,
                           last_updated_date)
-SELECT gen_random_uuid(),
+SELECT uuid_generate_v4(),
        user_id,
        'CASE_NUMBER',
        search_value,
