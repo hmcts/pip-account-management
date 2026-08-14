0b7a50b3d7d8f1c83fb6611e6bcef81ce2f80c79
HarveyMoj <harvey.prewer@justice.gov.uk>
2026-08-14 17:39:46 +0100

    Updating new url for email deletion

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/controllers/account/InactiveAccountManagementTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/account/management/controllers/account/InactiveAccountManagementTest.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class InactiveAccountManagementTest extends IntegrationTestBase {
     private static final String ROOT_URL = "/account";
     private static final String NOTIFY_INACTIVE_MEDIA_ACCOUNTS_URL = ROOT_URL + "/media/inactive/notify";
-    private static final String DELETE_EXPIRED_MEDIA_ACCOUNTS_URL = ROOT_URL + "/media/inactive";
+    private static final String DELETE_EXPIRED_MEDIA_ACCOUNTS_URL = ROOT_URL + "/media/inactive/deletion";
     private static final String DELETE_EXPIRED_ADMIN_ACCOUNTS_URL = ROOT_URL + "/admin/inactive";
     private static final String NOTIFY_INACTIVE_IDAM_ACCOUNTS_URL = ROOT_URL + "/idam/inactive/notify";
     private static final String DELETE_EXPIRED_IDAM_ACCOUNTS_URL = ROOT_URL + "/idam/inactive";
